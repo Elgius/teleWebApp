9b7ae60b9bc7e447495a40dd1949671d54893bc8
Elgius <114131653+Elgius@users.noreply.github.com>
2024-06-25 16:54:30 +0500

    port fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,17 +9,17 @@ import {
   bindThemeParamsCSSVars,
   bindViewportCSSVars,
   isSSR,
-} from '@tma.js/sdk-react';
-import { type FC, useEffect, useMemo } from 'react';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import { useRouter as useNavigationRouter } from 'next/navigation';
+} from "@tma.js/sdk-react";
+import { type FC, useEffect, useMemo } from "react";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useRouter as useNavigationRouter } from "next/navigation";
 
-import { useTelegramMock } from '@/hooks/useTelegramMock';
-import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { useTelegramMock } from "@/hooks/useTelegramMock";
+import { ErrorBoundary } from "@/components/ErrorBoundary";
 
-import './global.css';
+import "./global.css";
 
 const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
   <div>
@@ -28,9 +28,9 @@ const ErrorBoundaryError: FC<{ error: unknown }> = ({ error }) => (
       <code>
         {error instanceof Error
           ? error.message
-          : typeof error === 'string'
-            ? error
-            : JSON.stringify(error)}
+          : typeof error === "string"
+          ? error
+          : JSON.stringify(error)}
       </code>
     </blockquote>
   </div>
@@ -45,7 +45,7 @@ const BackButtonManipulator: FC = () => {
     if (!bb) {
       return;
     }
-    if (router.pathname === '/') {
+    if (router.pathname === "/") {
       bb.hide();
     } else {
       bb.show();
@@ -53,7 +53,7 @@ const BackButtonManipulator: FC = () => {
   }, [router, bb]);
 
   useEffect(() => {
-    return bb && bb.on('click', back);
+    return bb && bb.on("click", back);
   }, [bb, back]);
 
   return null;
@@ -78,45 +78,45 @@ const App: FC<AppProps> = ({ pageProps, Component }) => {
 
   return (
     <>
-      <BackButtonManipulator/>
-      <Component {...pageProps}/>
+      <BackButtonManipulator />
+      <Component {...pageProps} />
     </>
   );
 };
 
 const Inner: FC<AppProps> = (props) => {
   // Mock Telegram environment in development mode.
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useTelegramMock();
   }
 
   const debug = useMemo(() => {
-    return isSSR() ? false : retrieveLaunchParams().startParam === 'debug';
+    return isSSR() ? false : retrieveLaunchParams().startParam === "debug";
   }, []);
   const manifestUrl = useMemo(() => {
-    return isSSR() ? '' : new URL('tonconnect-manifest.json', window.location.href).toString();
+    return isSSR()
+      ? ""
+      : new URL("tonconnect-manifest.json", window.location.href).toString();
   }, []);
 
   useEffect(() => {
     if (debug) {
-      import('eruda').then(lib => lib.default.init());
+      import("eruda").then((lib) => lib.default.init());
     }
   }, [debug]);
 
   return (
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <SDKProvider acceptCustomStyles debug={debug}>
-        <App {...props}/>
-      </SDKProvider>
-    </TonConnectUIProvider>
+    <SDKProvider acceptCustomStyles debug={debug}>
+      <App {...props} />
+    </SDKProvider>
   );
 };
 
 export default function CustomApp(props: AppProps) {
   return (
     <ErrorBoundary fallback={ErrorBoundaryError}>
-      <Inner {...props}/>
+      <Inner {...props} />
     </ErrorBoundary>
   );
-};
+}
